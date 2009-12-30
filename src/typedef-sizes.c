158c1ce07a7bfb42fd8476cd108901b367e94767
David M. Syzdek <david@syzdek.net>
2009-12-30 14:10:10 -0900

    Adding ability to sort output in typedef-sizes.

--- src/typedef-sizes.c
+++ src/typedef-sizes.c
@@ -109,6 +109,14 @@
 #define MY_SIGN_WIDTH   8
 #define MY_FILE_WIDTH   0
 
+#define MY_SORT_REVERSE    0x1000
+#define MY_SORT_NAME       0x00
+#define MY_SORT_NAME_R     (MY_SORT_NAME|MY_SORT_REVERSE)
+#define MY_SORT_WIDTH      0x02
+#define MY_SORT_WIDTH_R    (MY_SORT_WIDTH|MY_SORT_REVERSE)
+#define MY_SORT_INCLUDE    0x04
+#define MY_SORT_INCLUDE_R  (MY_SORT_INCLUDE|MY_SORT_REVERSE)
+
 
 /////////////////
 //             //
@@ -133,6 +141,24 @@ struct my_data
 
 // main statement
 int main(int argc, char * argv[]);
+
+// compares by define
+int my_cmp_by_define(void * ptr1, void * ptr2);
+
+// compares by reverse define
+int my_cmp_by_define_r(void * ptr1, void * ptr2);
+
+// compares by name
+int my_cmp_by_name(void * ptr1, void * ptr2);
+
+// compares by reverse name
+int my_cmp_by_name_r(void * ptr1, void * ptr2);
+
+// compares by width
+int my_cmp_by_width(void * ptr1, void * ptr2);
+
+// compares by reverse width
+int my_cmp_by_width_r(void * ptr1, void * ptr2);
 
 // prints data record
 void my_print_data(struct my_data * data);
@@ -159,11 +185,15 @@ int main(int argc, char * argv[])
    int                     x;
    int                     y;
    int                     err;
+   int                     sort_order;
+   int                     sort_reverse;
    int                     opt_index;
+   int                    list_count;
    char                    msg[128];
    regex_t                 regex;
    regmatch_t              matches[2];
-   struct my_data        * ptr;
+   struct my_data       ** ptr;
+   struct my_data       ** list;
    static struct my_data   data[] =
    {
       { "char",                     sizeof(char),                     1,  NULL },
@@ -197,7 +227,7 @@ int main(int argc, char * argv[])
    };
 
    // getopt options
-   static char   short_opt[] = "htV";
+   static char   short_opt[] = "dhnrVw";
    static struct option long_opt[] =
    {
       {"help",          no_argument, 0, 'h'},
@@ -206,7 +236,11 @@ int main(int argc, char * argv[])
       {NULL,            0,           0, 0  }
    };
 
-   opt_index = 0;
+   list           = NULL;
+   list_count     = 0;
+   opt_index      = 0;
+   sort_order     = MY_SORT_NAME;
+   sort_reverse   = 0;
 
    while((c = getopt_long(argc, argv, short_opt, long_opt, &opt_index)) != -1)
    {
@@ -215,12 +249,24 @@ int main(int argc, char * argv[])
          case -1:	/* no more arguments */
          case 0:	/* long options toggles */
             break;
+         case 'd':
+            sort_order = MY_SORT_INCLUDE;
+            break;
          case 'h':
             my_usage();
             return(0);
+         case 'n':
+            sort_order = MY_SORT_NAME;
+            break;
+         case 'r':
+            sort_reverse = (sort_reverse ^ MY_SORT_REVERSE);
+            break;
          case 'V':
             my_version();
             return(0);
+         case 'w':
+            sort_order = MY_SORT_WIDTH;
+            break;
          case '?':
             fprintf(stderr, _("Try `%s --help' for more information.\n"), PROGRAM_NAME);
             return(1);
@@ -233,19 +279,17 @@ int main(int argc, char * argv[])
       };
    };
 
-   if ((opt_index+1) < argc)
-      opt_index++;
+   sort_order |= sort_reverse;
+
+   if (optind < argc)
+      optind++;
    else
-      argv[0] = ".*";
+   {
+      optind--;
+      argv[optind] = ".*";
+   };
 
-   printf("%-*s   %*s   %*s   %*s\n",
-      MY_NAME_WIDTH,  "name:",
-      MY_SIZE_WIDTH,  "width:",
-      MY_SIGN_WIDTH,  "signed:",
-      MY_FILE_WIDTH,  "defined"
-   );
-
-   for(x = opt_index; x < argc; x++)
+   for(x = optind; x < argc; x++)
    {
       ptr = NULL;
       if ((err = regcomp(&regex, argv[x], REG_EXTENDED|REG_ICASE)))
@@ -258,17 +302,223 @@ int main(int argc, char * argv[])
          if ( (!(err = regexec(&regex, data[y].name, 2, matches, 0))) ||
               (!(err = regexec(&regex, data[y].include, 2, matches, 0))) )
          {
-            ptr = &data[y];
-            my_print_data(ptr);
+            list_count++;
+            if (!(ptr = realloc(list, sizeof(struct my_data)*list_count)))
+            {
+               fprintf(stderr, PROGRAM_NAME ": out of virtual memory\n");
+               return(1);
+            };
+            list = ptr;
+            list[list_count-1] = &data[y];
          };
       regfree(&regex);
       if (!(ptr))
       {
          fprintf(stderr, PROGRAM_NAME ": data type `%s' not found\n", argv[x]);
+         if (list)
+            free(list);
          return(1);
       };
    };
+
+   switch(sort_order)
+   {
+      case MY_SORT_NAME:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_name);
+         break;
+      case MY_SORT_NAME_R:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_name_r);
+         break;
+      case MY_SORT_WIDTH:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_width);
+         break;
+      case MY_SORT_WIDTH_R:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_width_r);
+         break;
+      case MY_SORT_INCLUDE:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_define);
+         break;
+      case MY_SORT_INCLUDE_R:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_define_r);
+         break;
+      default:
+         qsort(list, list_count, sizeof(struct my_data *), (int (*)(const void *, const void *))my_cmp_by_name);
+         break;
+   };
+
+   printf("%-*s   %*s   %*s   %*s\n",
+      MY_NAME_WIDTH,  "name:",
+      MY_SIZE_WIDTH,  "width:",
+      MY_SIGN_WIDTH,  "signed:",
+      MY_FILE_WIDTH,  "defined"
+   );
+   for(x = 0; x < list_count; x++)
+      my_print_data(list[x]);
    printf("\n");
+
+   free(list);
+
+   return(0);
+}
+
+
+/// compares by define
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_define(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? 1 : -1);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp);
+
+   return(0);
+}
+
+
+/// compares by reverse define
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_define_r(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? -1 : 1);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp * -1);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp * -1);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp * -1);
+
+   return(0);
+}
+
+
+/// compares by name
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_name(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? 1 : -1);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp);
+
+   return(0);
+}
+
+
+/// compares by reverse name
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_name_r(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? -1 : 1);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp * -1);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp * -1);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp * -1);
+
+   return(0);
+}
+
+
+/// compares by width
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_width(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? 1 : -1);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp);
+
+   return(0);
+}
+
+
+/// compares by reverse width
+/// @param[in]  ptr1   pointer to first string
+/// @param[in]  ptr2   pointer to second string
+int my_cmp_by_width_r(void * ptr1, void * ptr2)
+{
+   int cmp;
+   const struct my_data * d1 = *((const struct my_data **)ptr1);
+   const struct my_data * d2 = *((const struct my_data **)ptr2);
+
+   if ((!(d1)) && (!(d2)))
+      return(0);
+   if ((!(d1)) || (!(d2)))
+      return(d1 ? -1 : 1);
+
+   if ((cmp = ((int)d1->size) - ((int)d2->size)))
+      return(cmp * -1);
+
+   if ((cmp = strcasecmp(d1->name, d2->name)))
+      return(cmp * -1);
+
+   if ((cmp = strcasecmp(d1->include, d2->include)))
+      return(cmp * -1);
 
    return(0);
 }
@@ -298,9 +548,13 @@ void my_usage()
    // line. The two strings referenced are: PROGRAM_NAME, and
    // PACKAGE_BUGREPORT
    printf(_("Usage: %s [options] [typedef]\n"
+         "  -d                        sort by object definition\n"
          "  -h, --help                print this help and exit\n"
+         "  -n                        sort by object name\n"
+         "  -r                        reverse sort order\n"
          "  -t type                   data type to display\n"
          "  -V, --version             print verbose messages\n"
+         "  -w                        sort by width of object\n"
          "\n"
          "Report bugs to <%s>.\n"
       ), PROGRAM_NAME, PACKAGE_BUGREPORT
