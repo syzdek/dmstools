ba749867cd7204635d8337eb63215bcec025b970
David M. Syzdek <syzdek@bindlebinaries.com>
2010-09-27 17:07:06 -0800

    Cleaning up code in colors.
    - removed escape character with \033
    - added descriptive comments to for loops
    - simplified loops for adding spacer rows
    - adjusted brackets.

--- src/colors.c
+++ src/colors.c
@@ -2,25 +2,32 @@
 
 int main(void);
 
-int main(void) {
+int main(void)
+{
+   int x;
+   int y;
 
-   int x,y;
-
-   for (y = 0; y < 9; y++) {
+   for (y = 0; y < 9; y++)
+   {
+      printf("\033[40m");
+      // displays odd rows of colors without bold
       for (x = 0; x < 9; x++)
-         printf(" [3%i;4%im  3%i;4%im  [40m", x, y, x, y);
-      printf("[0m\n");
+         printf(" \033[3%i;4%im  3%i;4%im  \033[40m", x, y, x, y);
+      printf(" \033[0m\n\033[40m");
+      // adds row of spaces
       for (x = 0; x < 9; x++)
-         printf(" [30;40m  30;40m  [40m");
-      printf("[0m\n");
+         printf(" \033[30;40m          ");
+      printf(" \033[0m\n\033[40m");
+      // displays even rows of colors with bold
       for (x = 0; x < 9; x++)
-         printf(" [1;3%i;4%im 1;3%i;4%im [40m", x, y, x, y);
-      printf("[0m\n");
+         printf(" \033[1;3%i;4%im 1;3%i;4%im \033[40m", x, y, x, y);
+      printf(" \033[0m\n\033[40m");
+      // adds row of spaces
       for (x = 0; x < 9; x++)
-         printf(" [30;40m  30;40m  [40m");
-      printf("[0m\n");
+         printf(" \033[30;40m          ");
+      printf(" \033[0m\n\033[40m");
    };
-   printf("[0m\n");
+   printf("\033[0m\n");
 
    return(0);
 };
