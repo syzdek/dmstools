8cbdf9c5b20b7f7f7054cc475ab98bd491936dae
David M. Syzdek <syzdek@bindlebinaries.com>
2013-12-02 14:11:48 -0900

    Adding autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+#   David M. Syzdek
+#   Copyright (C) 2013 David M. Syzdek <david@syzdek.net>.
+#
+#   @DMSTOOLS_BSD_LICENSE_START@
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are
+#   met:
+#
+#      * Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#      * Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#      * Neither the name of David M. Syzdek nor the
+#        names of its contributors may be used to endorse or promote products
+#        derived from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAVID M. SYZDEK BE LIABLE FOR
+#   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#   OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+#   SUCH DAMAGE.
+#
+#   @DMSTOOLS_BSD_LICENSE_END@
+#
+#   build-aux/autogen.sh - runs GNU Autotools to create build environment
+#
+
+AUTOGENNAME="`basename ${0}`" || exit $?
+SRCDIR=`dirname ${0}`
+
+
+cd $SRCDIR || exit 1
+
+if test -d .git || test -f .git;then
+   git submodule init           2>&1 | sed -e 's/^/git submodule init: /g' || exit 1
+   git submodule sync           2>&1 | sed -e 's/^/git submodule sync: /g' || exit 1
+   git submodule update --merge 2>&1 | sed -e 's/^/git submodule update: /g' || exit 1
+fi
+
+autoreconf -v -f -I m4 -I contrib/bindletools/m4 -W all -W error || exit 1
+
+# end of script
